6779a07111d70d4cc9c1ed2eae25dc77aabf0ecf
YUK3SH <yukeshwarr2@gmail.com>
2025-12-22 23:11:25 +0530

    make logger reload-safe for uvicorn

---
+++ backend/logger.py
@@ -0,0 +1,18 @@
+import logging
+import sys
+
+def setup_logger(name: str):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(message)s"
+    )
+    handler.setFormatter(formatter)
+
+    logger.handlers.clear()
+    logger.addHandler(handler)
+    logger.propagate = False
+
+    return logger
